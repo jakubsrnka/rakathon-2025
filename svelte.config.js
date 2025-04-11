013c156907791c6e20bf42bb8395fa4317797a97
Jakub Srnka <jakub.srnka@volny.cz>
2025-04-11 14:41:15 +0200

    🔧 svelte alias config

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,14 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
   preprocess: vitePreprocess(),
-  kit: { adapter: adapter() }
+  kit: {
+    adapter: adapter(),
+    alias: {
+      $components: 'src/components',
+      $types: 'src/lib/types',
+      $lib: 'src/lib'
+    }
+  }
 };
 
 export default config;
