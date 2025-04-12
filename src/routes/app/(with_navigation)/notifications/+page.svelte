100140f4e6156f4ab9a4333ddbd6a3c30984e5be
Jan Rehak <jan@ejhle.net>
2025-04-12 16:06:48 +0200

    ✨ update notifications with specific dates

--- src/routes/app/(with_navigation)/notifications/+page.svelte
+++ src/routes/app/(with_navigation)/notifications/+page.svelte
@@ -4,14 +4,15 @@
 </script>
 
 <Wrapper class="flex flex-col gap-4">
+  <h1 class="p-4 text-center text-3xl font-bold">Zprávy</h1>
+  <CustomNotification title="View flyer 3" content="" link="/app/flyers/3" date="2023-06-20" />
+  <CustomNotification title="View flyer 2" content="" link="/app/flyers/2" date="2023-05-15" />
+  <CustomNotification title="View flyer 1" content="" link="/app/flyers/1" date="2023-04-08" />
+  <CustomNotification title="Go to login page" content="" link="/login" date="2023-03-12" />
   <CustomNotification
     title="Mudr. Petr Novák Vám poslal zprávu"
     content="posílám vám informace k Vaší léčbě"
     link="/app/home"
-    date="Před 1 dnem"
+    date="2023-02-25"
   />
-  <CustomNotification title="Go to login page" content="" link="/login" />
-  <CustomNotification title="View flyer 1" content="" link="/app/flyers/1" />
-  <CustomNotification title="View flyer 2" content="" link="/app/flyers/2" />
-  <CustomNotification title="View flyer 3" content="" link="/app/flyers/3" />
 </Wrapper>
