a125007f1ff62ef24e22c002512172eebc1120f4
Jan Rehak <jan@ejhle.net>
2025-04-12 20:08:55 +0200

    ✨ add dark mode support

--- src/routes/app/(with_navigation)/+layout.svelte
+++ src/routes/app/(with_navigation)/+layout.svelte
@@ -10,18 +10,18 @@
 <div class="mx-auto max-w-screen-sm">
   {@render children()}
   <div
-    class="fixed inset-x-0 bottom-0 flex h-12 items-center justify-between border-t border-gray-200 bg-white px-4 shadow-md"
+    class="fixed inset-x-0 bottom-0 flex h-12 items-center justify-between border-t border-border px-4 shadow-md"
     class:shadow={currentPathSegments[1] !== 'home'}
   >
     <div class="mx-auto flex h-full w-full max-w-screen-sm items-center justify-between">
       <a href="/app/home" class="flex h-full w-full items-center justify-center">
-        <Home color={currentPathSegments[1] === 'home' ? 'black' : 'grey'} />
+        <Home class={currentPathSegments[1] === 'home' ? 'text-primary' : 'text-muted-foreground'} />
       </a>
       <a href="/app/notifications" class="flex h-full w-full items-center justify-center">
-        <MessageSquare color={currentPathSegments[1] === 'notifications' ? 'black' : 'grey'} />
+        <MessageSquare class={currentPathSegments[1] === 'notifications' ? 'text-primary' : 'text-muted-foreground'} />
       </a>
       <a href="/app/settings" class="flex h-full w-full items-center justify-center">
-        <Settings color={currentPathSegments[1] === 'settings' ? 'black' : 'grey'} />
+        <Settings class={currentPathSegments[1] === 'settings' ? 'text-primary' : 'text-muted-foreground'} />
       </a>
     </div>
   </div>
