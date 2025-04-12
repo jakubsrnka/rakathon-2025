915caf5a784ea8ed862eba833248c131f1bbc8b9
Jan Rehak <jan@ejhle.net>
2025-04-12 02:49:14 +0200

    💄 improve optional parameters of Notification

--- src/components/app/Notification.svelte
+++ src/components/app/Notification.svelte
@@ -8,16 +8,38 @@
     children,
     link,
     title,
-    content
-  }: { class?: string; children?: Snippet; link: string; title: string; content: string } = $props();
+    content,
+    date
+  }: {
+    class?: string;
+    children?: Snippet;
+    link: string;
+    title: string;
+    content?: string;
+    date?: string;
+  } = $props();
 </script>
 
 <div class={cn('', className)}>
   <a href={link}>
     <Alert.Root class="shadow">
-      <Bell class="h-4 w-4" />
-      <Alert.Title>{title}</Alert.Title>
-      <Alert.Description>{content}</Alert.Description>
+      <div class="flex items-center">
+        <div class="flex-shrink-0">
+          <!-- TODO add more icons -->
+          <Bell class="h-4 w-4" />
+        </div>
+        <div class="ml-4">
+          {#if content}
+            <Alert.Title class="mb-1">{title}</Alert.Title>
+            <Alert.Description>{content}</Alert.Description>
+          {:else}
+            <Alert.Title>{title}</Alert.Title>
+          {/if}
+          {#if date}
+            <Alert.Description class="italic">{date}</Alert.Description>
+          {/if}
+        </div>
+      </div>
     </Alert.Root>
   </a>
 </div>
