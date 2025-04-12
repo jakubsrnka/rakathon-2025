051aa7be65f7aeb4b215f79c75a02f227d848f17
Jan Rehak <jan@ejhle.net>
2025-04-12 13:40:16 +0200

    ✨ add path to specific flyers and mock 3 basic ones

--- src/components/ui/progress/progress.svelte
+++ src/components/ui/progress/progress.svelte
@@ -15,7 +15,7 @@
   {...$$restProps}
 >
   <div
-    class="h-full w-full flex-1 bg-primary transition-all"
+    class="linear h-full w-full flex-1 bg-primary transition-all duration-[100ms]"
     style={`transform: translateX(-${100 - (100 * (value ?? 0)) / (max ?? 1)}%)`}
   ></div>
 </ProgressPrimitive.Root>
