2b221615645e5871f1094b20dd0d23d5213500e6
Jan Rehak <jan@ejhle.net>
2025-04-13 10:24:23 +0200

    ✨ introduced notification fetching

--- src/routes/(restricted)/app/(with_navigation)/notifications/+page.svelte
+++ src/routes/(restricted)/app/(with_navigation)/notifications/+page.svelte
@@ -4,29 +4,42 @@
   import Skeleton from '$components/ui/skeleton/skeleton.svelte';
   import Wrapper from '$components/Wrapper.svelte';
   import { m } from '$lib/paraglide/messages';
+  import { currentUser, pbClient } from '$lib/pocketbase';
+  import { Collections, type FlyersResponse, type NotificationsResponse } from '$types/pocketbase';
 
-  export async function load() {
-    await new Promise((resolve) => setTimeout(resolve, 1000)); // Simulates a 1-second wait
-  }
+  const loadNotifications = async () => {
+    try {
+      if (!currentUser) {
+        throw new Error('User not logged in');
+      }
+
+      return pbClient
+        .collection(Collections.Notifications)
+        .getFullList<NotificationsResponse<{ flyer: FlyersResponse }>>({
+          expand: 'flyer'
+        });
+    } catch (error) {
+      console.error('Error fetching notifications:', error);
+      throw error;
+    }
+  };
 </script>
 
 <Wrapper class="flex flex-col gap-4">
   <PageHeading>{m.app_notifications_heading()}</PageHeading>
-  {#await load()}
+  {#await loadNotifications()}
     {#each Array(6) as _}
       <Skeleton class="h-6 w-32 rounded-lg p-3" />
       <Skeleton class="h-12 w-full rounded-lg p-3" />
     {/each}
-  {:then}
-    <CustomNotification title="View flyer 3" content="" link="/app/flyers/3" date="2023-06-20" />
-    <CustomNotification title="View flyer 2" content="" link="/app/flyers/2" date="2023-05-15" />
-    <CustomNotification title="View flyer 1" content="" link="/app/flyers/1" date="2023-04-08" />
-    <CustomNotification title="Go to login page" content="" link="/login" date="2023-03-12" />
-    <CustomNotification
-      title="Mudr. Petr Novák Vám poslal zprávu"
-      content="posílám vám informace k Vaší léčbě"
-      link="/app/home"
-      date="2023-02-25"
-    />
+  {:then notifications}
+    {#each notifications as notification}
+      <CustomNotification
+        title={notification.text}
+        content={notification.expand?.flyer?.title ?? ''}
+        link={notification.expand?.flyer ? `/app/flyers/${notification.expand.flyer.id}` : ''}
+        date={notification.created}
+      />
+    {/each}
   {/await}
 </Wrapper>
